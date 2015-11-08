5f9a98d595d2c3fb8d1e0e7a42d4950960d0bf63
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-11-08 16:02:20 -0600

    first json/ajax

--- wiki.php
+++ wiki.php
@@ -5,7 +5,6 @@
 </head>
 <body>
     <style>
-
       form {
         border: 10px solid black;
         margin: 20px;
@@ -18,7 +17,7 @@
 
       textarea{
         width: 100%;
-        height: 90%; /* Change this if we add more input */
+        height: 30%; /* Change this if we add more input */
       }
 
       #save{
@@ -30,13 +29,6 @@
       }
 
     </style>
-    <script>
-      $('#content').click(function() { //What does this do?  There is no element with id "content"!
-        $('form').removeClass('hidden'); //Makes the form visible
-        $('#content').addClass('hidden'); //Makes whatever has id "content" invisible
-      }
-    );
-    </script>
     <?php
 
     if (file_exists('wiki.txt')) {
@@ -53,9 +45,32 @@
 
     $s_content = htmlentities($content); //Escape code in text file to negate possible attack vector
 
-     ?>
-     <form class="hidden" action="wiki.php">
+    ?>
+    <script>
+      $('#content').click(function() { //What does this do?  There is no element with id "content"!
+        $('form').removeClass('hidden'); //Makes the form visible
+        $('#content').addClass('hidden'); //Makes whatever has id "content" invisible
+      }
+
+      $('#data').submit(function() {
+         var data = $('data'); //The form data we're submitting
+         $.ajax({
+           url: 'wiki.php', //We're submitting it to wiki.php
+           data: data, //The data we're sending it
+           dataType: 'json', //Sending it as a JSON
+           type: 'POST', //via POST
+           async: false, //Not asynchronous...
+           success: function(data){
+             alert("Success");
+           }
+         })
+      })
+    );
+    </script>
+     <form id="data" class="hidden" action="wiki.php">
        <textarea name="content" rows="8" cols="80"><?php echo $safe_content; ?></textarea>
+       <textarea name="content2" rows="8" cols="80"><?php echo $safe_content; ?></textarea>
+       <textarea name="content3" rows="8" cols="80"><?php echo $safe_content; ?></textarea>
        <input id="save" type="submit" value="Save">
      </form>
 </body>
