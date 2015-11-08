27784733392496f531c1c1e618d3398f4d119f99
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-11-08 16:22:23 -0600

    added class hidden

--- wiki.php
+++ wiki.php
@@ -29,23 +29,6 @@
       }
 
     </style>
-    <?php
-
-    if (file_exists('wiki.txt')) {
-      $content = file_get_contents('wiki.txt'); //Reads content from wiki.txt and puts it into $content
-    }
-    else if (isset($_GET['content'])) //If there's stuff in ?content=
-    {
-       $content = $_GET['content']; //Assign $content to that stuff
-       file_put_contents('wiki.txt', $content); //Then slaps $contents into wiki.txt
-    }
-    else {
-      $content = '(no content)'; //No content by default if wiki.txt is missing
-    }
-
-    $s_content = htmlentities($content); //Escape code in text file to negate possible attack vector
-
-    ?>
     <script>
       $('#content').click(function() { //What does this do?  There is no element with id "content"!
         $('form').removeClass('hidden'); //Makes the form visible
@@ -68,7 +51,24 @@
       })
     );
     </script>
-     <form id="data" action="wiki.php">
+    <?php
+
+    if (file_exists('wiki.txt')) {
+      $content = file_get_contents('wiki.txt'); //Reads content from wiki.txt and puts it into $content
+    }
+    else if (isset($_GET['content'])) //If there's stuff in ?content=
+    {
+       $content = $_GET['content']; //Assign $content to that stuff
+       file_put_contents('wiki.txt', $content); //Then slaps $contents into wiki.txt
+    }
+    else {
+      $content = '(no content)'; //No content by default if wiki.txt is missing
+    }
+
+    $s_content = htmlentities($content); //Escape code in text file to negate possible attack vector
+
+    ?>
+     <form id="data" class="hidden" action="wiki.php">
        <textarea name="content" rows="8" cols="80"><?php echo $safe_content; ?></textarea>
        <textarea name="content2" rows="8" cols="80"><?php echo $safe_content; ?></textarea>
        <textarea name="content3" rows="8" cols="80"><?php echo $safe_content; ?></textarea>
