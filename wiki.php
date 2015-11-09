0d0b4fa33c4970a527faa848338d60c3cdb6f5b0
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-11-09 15:31:47 -0600

    wrap in ready function

--- wiki.php
+++ wiki.php
@@ -27,23 +27,25 @@
         $('.content').addClass('hidden'); //Makes whatever has id "content" invisible
       })
 
-      $('#data').submit(function(event) {
-
-        event.preventDefault();
-        alert("Hi");
-         var data = $('#data'); //The form data we're submitting
-         $.ajax({
-           url: 'wiki.php', //We're submitting it to wiki.php
-           data: data, //The data we're sending it
-           dataType: 'json', //Sending it as a JSON
-           type: 'POST', //via POST
-           async: false, //Not asynchronous...
-           success: function(data){
-             alert("Success");
-           }
-         })
-      })
-    );
+      $(function() { //Wrap in ready function because apparently 
+        $('#data').submit(function(event) {
+          event.preventDefault();
+          alert("Hi");
+           var data = $('#data'); //The form data we're submitting
+           $.ajax({
+             url: 'wiki.php', //We're submitting it to wiki.php
+             data: data, //The data we're sending it
+             contentType: 'application/json; charset=UTF-8', //Make sure we're actually sending a JSON and not just a query-string...
+             dataType: 'json', //Sending it as a JSON
+             type: 'POST', //via POST
+             async: false, //Not asynchronous...
+             success: function(data){
+               alert("Success");
+             }
+           })
+        })
+      );
+    })
     </script>
     <?php
 
