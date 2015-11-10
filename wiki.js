c08ccb2124c01deace2dcd517aa46a26b1be3a9b
IntroToScriptingLanguages <students@GCCMACBOOKA-3.local>
2015-11-10 13:01:27 -0600

    what I got

--- wiki.js
+++ wiki.js
@@ -3,32 +3,22 @@ $('.content').click(function() { //What does this do?  There is no element with 
   $('.content').addClass('hidden'); //Makes whatever has id "content" invisible
 });
 
-$('#debug').html("Hello!");
-
-document.getElementById('debug').innerHTML = "Hello! JQUERY Isn't working!";
-
 $(function() { //Wrap in ready function because apparently
-  $('#debug').html("Wazzup!");
-
   $('#data').submit(function(event) {
     event.preventDefault();
-    $('#debug').html("Submitting!");
+    $('#response').html("Hi!");
      var data = $('#data').serialize(); //The form data we're submitting
      $.ajax({
-       url: 'wiki.php', //We're submitting it to wiki.php
+       url: 'wiki.html', //We're submitting it to wiki.php
        data: data, //The data we're sending it
-       dataType: 'json', //Sending it as a JSON
        type: 'POST', //via POST
        async: true, //Not asynchronous...
-       success: function(data){ //Change data here
-         $('#debug').html("Success!");
+       success: function(n_data){ //Change data here
+         $('#response').html("Yay!");
        },
-       error: function(data){
-         $('#debug').html("Error ".concat(data.serialize()));
+       error: function(n_data){
+         $('#response').html("Error!");
        },
-       complete: function(xhr, text){
-          $('#debug').html("Finished with code ".concat(xhr.status).concat(", ").concat(text));
-       }
      });
   });
 });
