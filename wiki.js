cc647b789891ac3d1a05cac420db819839ea0159
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-11-11 17:32:40 -0600

    xcvcxxcv

--- wiki.js
+++ wiki.js
@@ -19,24 +19,28 @@ $(function() { //Wrap in ready function because apparently
     $('#debug').html("Submitting!");
      var form_data = $('#data').serialize(); //The form data we're submitting
 
-     $.ajax({url: 'wiki.php?string='.concat(form_data)}); //Writes form data to wiki.txt
+     $.ajax({url: 'wiki.php?string='.concat(form_data)
+        success: function(new_data){
+          $.ajax({
+            url: 'wiki.html', //We're submitting it to wiki.php
+            data: new_data, //The data we're sending it
+            type: 'POST', //via POST
+            async: true, //Not asynchronous...
+            success: function(d, stat){ //Add code that changes content1-3 here
+              $('#debug').html("Success! ".concat(stat));
+              $('form').addClass('hidden'); //Makes the form visible
+              $('#content').html(getParameter('content', d));
+              $('#content2').html(getParameter('content2', d));
+              $('#content3').html(getParameter('content3', d));
+            },
+            error: function(data, stat){
+              $('#debug').html("Error ".concat(form_data));
+              $('form').addClass('hidden'); //Makes the form visible
+            }
+         });
+        }
+      }); //Writes form data to wiki.txt
 
-     $.ajax({
-       url: 'wiki.txt', //We're submitting it to wiki.php
-       data: form_data, //The data we're sending it
-       type: 'POST', //via POST
-       async: true, //Not asynchronous...
-       success: function(d, stat){ //Add code that changes content1-3 here
-         $('#debug').html("Success! ".concat(stat));
-         $('form').addClass('hidden'); //Makes the form visible
-         $('#content').html(getParameter('content', d));
-         $('#content2').html(getParameter('content2', d));
-         $('#content3').html(getParameter('content3', d));
-       },
-       error: function(data, stat){
-         $('#debug').html("Error ".concat(form_data));
-         $('form').addClass('hidden'); //Makes the form visible
-       }
-    });
+
   });
 });
