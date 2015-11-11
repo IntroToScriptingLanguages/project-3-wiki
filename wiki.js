dd35c4e5841aa2c4338a61ed5e58bcd58e8079de
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-11-11 16:02:20 -0600

    implemented content changing

--- wiki.js
+++ wiki.js
@@ -7,25 +7,39 @@ $('#debug').html("Hello!");
 
 document.getElementById('debug').innerHTML = "Hello! JQUERY Isn't working!";
 
+function getParameter(param, queryString) //Gets parameter from uery string
+{
+   var string = queryString;
+   var stringArray = queryString.split('&');
+   $.each(stringArray, function(i, v) {
+     var paramArray = v.split('=');
+     if (paramArray[0] == param)
+        return paramArray[1];
+   })
+   return null;
+}
+
 $(function() { //Wrap in ready function because apparently
   $('#debug').html("Wazzup!");
 
   $('#data').submit(function(event) {
     event.preventDefault();
     $('#debug').html("Submitting!");
-     var data = $('#data'); //The form data we're submitting
+     var form_data = $('#data').serialize(); //The form data we're submitting
      $.ajax({
-       url: 'wiki.php', //We're submitting it to wiki.php
-       data: data, //The data we're sending it
+       url: 'wiki.html', //We're submitting it to wiki.php
+       data: form_data, //The data we're sending it
        contentType: 'application/json; charset=UTF-8', //Make sure we're actually sending a JSON and not just a query-string...
-       dataType: 'json', //Sending it as a JSON
        type: 'POST', //via POST
        async: true, //Not asynchronous...
-       success: function(data){ //Change data here
-         $('#debug').html("Success!");
+       success: function(data, stat){ //Add code that changes content1-3 here
+         $('#debug').html("Success! ".concat(stat));
+         $('#content').html(getParameter('content', data));
+         $('#content2').html(getParameter('content2', data));
+         $('#content3').html(getParameter('content3', data));
        },
-       error: function(data){
-         $('#debug').html("Error");
+       error: function(data, stat){
+         $('#debug').html("Error ".concat(form_data));
        }
      });
   });
