eee75a8a23821e645c2e59626d8c051c932f7275
IntroToScriptingLanguages <zahntastic@gmail.com>
2015-12-03 12:00:26 -0600

    send data

--- wiki.js
+++ wiki.js
@@ -1,19 +1,21 @@
-function getParameter(param, queryString) //Gets parameter from uery string
-{
-   var string = queryString;
-   console.log("queryString: ".concat(queryString));
-   var stringArray = queryString.split('&');
-   $.each(stringArray, function(i, v) {
-     var paramArray = v.split('=');
-
-     console.log(v);
-     if (paramArray[0] == param)
-        return paramArray[1];
-   });
-   return null;
-}
-
 jQuery(document).ready(function($) {
+
+   function getParameter(param, queryString) //Gets parameter from uery string
+   {
+      var string = queryString;
+      console.log("queryString: ".concat(queryString));
+      var stringArray = queryString.split('&');
+      $.each(stringArray, function(i, v) {
+        var paramArray = v.split('=');
+   
+        console.log(v);
+        if (paramArray[0] == param)
+           return paramArray[1];
+      });
+      return null;
+   }
+
+
   $('#data').submit(function(event) {
     event.preventDefault();
     $('#debug').html("Submitting!");
@@ -22,14 +24,13 @@ jQuery(document).ready(function($) {
           console.log("formdata: ".concat(form_data));
 
             $.ajax({
-              url: 'wiki.php', //We're submitting it to wiki.php
               data: form_data, //The data we're sending it
               type: 'POST', //via GET
               success: function(d, stat){ //Add code that changes content1-3 here
                 $('#debug').html("Success! ".concat(d));
               },
               error: function(data, stat){
-                $('#debug').html("Error ".concat(form_data).concat(stat));
+                $('#debug').html("Error ".concat(form_data)..concat(' ').concat(stat));
      					}
            });
  //Writes form data to wiki.txt
